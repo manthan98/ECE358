309f9794c12fbe15641e0f5c9c235f982c435568
Manthan Shah <manthan711@gmail.com>
2020-10-15 16:44:23 -0400

    Add some optimizations

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,5 +1,6 @@
 import random
 import math
+import collections
 
 def generate_random_variable(l=12):
     u = random.uniform(0, 1)
@@ -7,45 +8,24 @@ def generate_random_variable(l=12):
     return x
 
 class Node:
-    def __init__(self, arrival_times, collisions):
+    def __init__(self, arrival_times, collisions, latest_arrival_time):
         self.arrival_times = arrival_times
         self.collisions = collisions
+        self.latest_arrival_time = latest_arrival_time
 
 def generate_packet_arrivals(num_nodes, T_sim):
     nodes = []
     for i in range(num_nodes):
         curr_time = 0
-        arrival_times = []
+        arrival_times = collections.deque([])
 
         while curr_time < T_sim:
             curr_time += generate_random_variable()
             if curr_time < T_sim:
                 arrival_times.append(curr_time)
 
-        arrival_times.reverse()
-        nodes.append(Node(arrival_times, 0))
-
+        nodes.append(Node(arrival_times, 0, arrival_times[0], float('-inf')))
     return nodes
-
-def find_min_queue(nodes):
-    smallest_arrivals = []
-    for node in nodes:
-        if not len(node.arrival_times) > 0:
-            continue
-        smallest_arrivals.append(node.arrival_times[-1])
-
-    if len(smallest_arrivals) == 0:
-        return None
-
-    smallest_arrival = min(smallest_arrivals)
-    for i in range(len(nodes)):
-        if not len(nodes[i].arrival_times) > 0:
-            continue
-
-        if nodes[i].arrival_times[-1] == smallest_arrival:
-            return i
-
-    return None
 
 def persistent_csma_cd(nodes, T_sim, D, C, S, L, R):
     total_tx = 0
@@ -54,26 +34,15 @@ def persistent_csma_cd(nodes, T_sim, D, C, S, L, R):
     curr_time = 0
     min_queue_idx = 0
     while curr_time < T_sim:
-        # Determine queue with lowest arrival packet time stamp
-        smallest_arrivals = []
-        for node in nodes:
-            if not len(node.arrival_times) > 0:
-                continue
-            smallest_arrivals.append(node.arrival_times[-1])
+        curr_time = float('inf')
+        for i in range(len(nodes)):
+            if nodes[i].latest_arrival_time < curr_time:
+                curr_time = nodes[i].latest_arrival_time
+                min_queue_idx = i
 
-        if len(smallest_arrivals) == 0:
+        if curr_time == float('inf'):
             break
 
-        smallest_arrival = min(smallest_arrivals)
-        for i in range(len(nodes)):
-            if not len(nodes[i].arrival_times) > 0:
-                continue
-
-            if nodes[i].arrival_times[-1] == smallest_arrival:
-                min_queue_idx = i
-                break
-
-        curr_time = smallest_arrival
         print(curr_time)
 
         collision_occurred = False
@@ -84,26 +53,31 @@ def persistent_csma_cd(nodes, T_sim, D, C, S, L, R):
                 continue
 
             T_prop = (D / S) * abs(i - min_queue_idx)
-            T_first_bit = nodes[min_queue_idx].arrival_times[-1] + T_prop
-            T_final_bit = nodes[min_queue_idx].arrival_times[-1] + T_prop + (L / R)
+            T_first_bit = nodes[min_queue_idx].arrival_times[0] + T_prop
+            T_final_bit = nodes[min_queue_idx].arrival_times[0] + T_prop + (L / R)
 
             if not len(nodes[i].arrival_times) > 0:
                 continue
 
-            if nodes[i].arrival_times[-1] < T_first_bit:
+            if nodes[i].arrival_times[0] < T_first_bit:
                 collision_occurred = True
                 nodes[i].collisions += 1
 
                 if nodes[i].collisions > 10:
-                    nodes[i].arrival_times.pop()
+                    nodes[i].arrival_times.popleft()
                     nodes[i].collisions = 0
+                    if len(nodes[i].arrival_times) > 0:
+                        nodes[i].latest_arrival_time = nodes[i].arrival_times[0]
+                    else:
+                        nodes[i].latest_arrival_time = float('inf')
                     continue
 
                 T_backoff = random.randint(0, (2**nodes[i].collisions) - 1) * (512 / R)
                 T_wait = T_final_bit + T_backoff
-                for j in reversed(range(len(nodes[i].arrival_times))):
+                for j in range(len(nodes[i].arrival_times)):
                     if nodes[i].arrival_times[j] < T_wait:
                         nodes[i].arrival_times[j] = T_wait
+                        nodes[i].latest_arrival_time = T_wait
                         total_tx += 1
                     else:
                         break
@@ -112,27 +86,38 @@ def persistent_csma_cd(nodes, T_sim, D, C, S, L, R):
             collision_occurred = False
             T_backoff = random.randint(0, (2**nodes[min_queue_idx].collisions) - 1) * (512 / R)
             T_prop_furthest = (D / S) * abs(max(i - len(nodes), i - 0))
-            T_furthest = nodes[min_queue_idx].arrival_times[-1] + T_prop_furthest + (L / R) + T_backoff
+            T_furthest = nodes[min_queue_idx].arrival_times[0] + T_prop_furthest + (L / R) + T_backoff
 
             nodes[min_queue_idx].collisions += 1
             if nodes[min_queue_idx].collisions <= 10:
-                for i in reversed(range(len(nodes[min_queue_idx].arrival_times))):
+                for i in range(len(nodes[min_queue_idx].arrival_times)):
                     if nodes[min_queue_idx].arrival_times[i] < T_furthest:
                         nodes[min_queue_idx].arrival_times[i] = T_furthest
+                        nodes[min_queue_idx].latest_arrival_time = T_furthest
                     else:
                         break
             else:
-                nodes[min_queue_idx].arrival_times.pop()
+                nodes[min_queue_idx].arrival_times.popleft()
                 nodes[min_queue_idx].collisions = 0
+                if len(nodes[min_queue_idx].arrival_times) > 0:
+                    nodes[min_queue_idx].latest_arrival_time = nodes[min_queue_idx].arrival_times[0]
+                else:
+                    nodes[min_queue_idx].latest_arrival_time = float('inf')
         else:
             for i in range(len(nodes)):
                 # Packet arrives at node during an ongoing transmission
-                for j in reversed(range(len(nodes[i].arrival_times))):
+                for j in range(len(nodes[i].arrival_times)):
                     if nodes[i].arrival_times[j] > T_first_bit and nodes[i].arrival_times[j] < T_final_bit:
                         nodes[i].arrival_times[j] = T_final_bit
-            
-            nodes[min_queue_idx].arrival_times.pop()
+                if len(nodes[i].arrival_times) > 0:
+                    nodes[i].latest_arrival_time = nodes[i].arrival_times[0]
+
+            nodes[min_queue_idx].arrival_times.popleft()
             nodes[min_queue_idx].collisions = 0
+            if len(nodes[min_queue_idx].arrival_times) > 0:
+                nodes[min_queue_idx].latest_arrival_time = nodes[min_queue_idx].arrival_times[0]
+            else:
+                nodes[min_queue_idx].latest_arrival_time = float('inf')
             successful_tx += 1
         
         total_tx += 1
@@ -145,7 +130,7 @@ S = (2 / 3) * C
 D = 10
 L = 1500
 R = 10**6
-nodes = generate_packet_arrivals(20, 100)
+nodes = generate_packet_arrivals(100, 100)
 persistent_csma_cd(nodes, 100, D, C, S, L, R)
 
                 
